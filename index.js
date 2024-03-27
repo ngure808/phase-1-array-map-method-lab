f533f4e690dd1b64fe58c63a193aaffa0496c07a
ngure808 <mwangingure808@gmail.com>
2024-03-27 09:02:39 +0300

    map-mathod

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased () {
+
+  return tutorials.map(string => {
+    const words = string.split(" ");
+    const capitalizedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+      });
+    return capitalizedWords.join(" ")
+});
 }
+titleCased()
